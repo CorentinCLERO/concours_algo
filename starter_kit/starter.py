1c86a3a5413071ace3d3a89c7ed7c325ab98be09
CorentinCLERO <corentin.clero@epsi.fr>
2025-01-15 19:08:02 +0100

    push config to try

--- starter_kit/starter.py
+++ starter_kit/starter.py
@@ -2,15 +2,15 @@ from src.analyser import Analyser
 from src.loop import Loop
 
 # dataset_file = "1_example"
-# dataset_file = "2_pacman"
+dataset_file = "2_pacman"
 # dataset_file = "3_efrei"
 # dataset_file = "4_manhattan"
 # dataset_file = "5_gta"
-dataset_file = "6_paris"
+# dataset_file = "6_paris"
 # dataset_file = "7_london"
 dataset = open(f'.\\datasets\\{dataset_file}.json').read()
 depth_number = 10
-max_attempts = 1
+max_attempts = 10
 print('---------------------------------')
 print(f'Solving {dataset_file}')
 Loop.loop(dataset, dataset_file, max_attempts, depth_number)
