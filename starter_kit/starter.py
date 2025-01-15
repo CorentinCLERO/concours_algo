8301a3638981290591677cecfe8d4c103089c76a
CorentinCLERO <corentin.clero@epsi.fr>
2025-01-15 19:17:57 +0100

    push score

--- starter_kit/starter.py
+++ starter_kit/starter.py
@@ -1,5 +1,6 @@
 from src.analyser import Analyser
 from src.loop import Loop
+import time
 
 # dataset_file = "1_example"
 dataset_file = "2_pacman"
@@ -10,10 +11,14 @@ dataset_file = "2_pacman"
 # dataset_file = "7_london"
 dataset = open(f'.\\datasets\\{dataset_file}.json').read()
 depth_number = 10
-max_attempts = 10
+max_attempts = 100
 print('---------------------------------')
 print(f'Solving {dataset_file}')
+start_time = time.time()
 Loop.loop(dataset, dataset_file, max_attempts, depth_number)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Execution time: {elapsed_time:.2f} seconds")
 
 # Analyser un dataset spécifique
 # Analyser.analyze_history(max_attempts, dataset, dataset_file)
